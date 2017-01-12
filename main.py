28a78817f8aef084173e9b686d957ae00546c29f
Antoine Dozois <dozois.a@gmail.com>
2017-01-12 22:33:15 +0100

    travis build test

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from knn.core.classifier import MultiProcessedKnnClassifier as multi_processed_k
 if __name__ == '__main__':
     training_data_set = DatasetFactory.create_dataset_from_files(data_type='training')
     test_data_set = DatasetFactory.create_dataset_from_files(data_type="test")
+    
 
     # knn.classify(training_data_set=training_data_set, test_data_set=test_data_set, number_of_neighbors=10)
     multi_processed_knn(training_data_set=training_data_set).classify(test_data_set=test_data_set,
