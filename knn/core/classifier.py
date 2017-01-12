ae9a9ee5ad64f93e7405859c870cad43e33e0be4
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2017-01-12 17:15:24 +0100

    + Added multi process knn classifier

--- knn/core/classifier.py
+++ knn/core/classifier.py
@@ -1,17 +1,96 @@
+import multiprocessing
+from multiprocessing import Process
+from multiprocessing.managers import BaseManager
+
 from commons.models.classificationStats import ClassificationStats
 from knn.models.model import Model
+import numpy as np
 
 
 class KnnClassifier(object):
     @staticmethod
-    def classify(training_dataset, test_dataset, number_of_neighbors):
+    def classify(training_data_set, test_data_set, number_of_neighbors):
         print("KNN classification in progess... \n")
         classification_stats = ClassificationStats(ClassificationStats.CLASSIFICATION_METHODS["KNN"])
-        knn_model = Model().train_with_dataset(training_dataset)
+        knn_model = Model().train_with_dataset(training_data_set)
 
-        for data_instance in test_dataset.data_instances[:5]:
+        classification_stats.set_classification_start_time()
+        for data_instance in test_data_set.data_instances[:5]:
             estimated_label = knn_model.classify(data_instance, number_of_neighbors=number_of_neighbors)
             correctly_classified = data_instance.label == estimated_label
             classification_stats.register_data_instance_classification(label=data_instance.label,
                                                                        correctly_classified=correctly_classified)
-        print(classification_stats)
+        print(classification_stats.to_string())
+
+
+class MultiProcessedKnnClassifier(object):
+    __number_of_neighbors = 0
+    __process_manager = None
+    __classification_stats = None
+    __knn_model = None
+
+    def __init__(self, training_data_set):
+        ProcessManager.register('ClassificationStats', ClassificationStats)
+        self.__process_manager = ProcessManager()
+        self.__process_manager.start()
+        self.__classification_stats = self.__process_manager.ClassificationStats(
+                ClassificationStats.CLASSIFICATION_METHODS["PKNN"])
+        self.__knn_model = Model().train_with_dataset(training_data_set)
+
+    @property
+    def knn_model(self):
+        return self.__knn_model
+
+    @property
+    def classification_stats(self):
+        return self.__classification_stats
+
+    @property
+    def number_of_neighbors(self):
+        return self.__number_of_neighbors
+
+    def classify(self, test_data_set, number_of_neighbors):
+        print("KNN classification in progess... \n")
+        self.__number_of_neighbors = number_of_neighbors
+
+        classification_threads = map(
+                lambda data_instances: ClassificationProcess(self.__knn_model, data_instances,
+                                                             self.__classification_stats, number_of_neighbors),
+                np.array_split(test_data_set.data_instances[:5], multiprocessing.cpu_count()))
+
+        self.__classification_stats.set_classification_start_time()
+
+        for classification_thread in classification_threads:
+            classification_thread.start()
+
+        for classification_thread in classification_threads:
+            classification_thread.join()
+
+        print(self.__classification_stats.to_string())
+
+
+class ClassificationProcess(Process):
+    __number_of_neighbors = 0
+    __classification_stats = None
+    __test_data_instances = None
+    __knn_model = None
+
+    def __init__(self, knn_model, test_data_instances, classification_stats, number_of_neighbors):
+        super(ClassificationProcess, self).__init__()
+        self.__knn_model = knn_model
+        self.__classification_stats = classification_stats
+        self.__test_data_instances = test_data_instances
+        self.__number_of_neighbors = number_of_neighbors
+
+    def run(self):
+        for data_instance in self.__test_data_instances:
+            estimated_label = self.__knn_model.classify(
+                    data_instance,
+                    number_of_neighbors=self.__number_of_neighbors)
+            correctly_classified = data_instance.label == estimated_label
+            self.__classification_stats.register_data_instance_classification(label=data_instance.label,
+                                                                              correctly_classified=correctly_classified)
+
+
+class ProcessManager(BaseManager):
+    pass
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from commons.models.datasetFactory import DatasetFactory
 from knn.core.classifier import KnnClassifier as knn
+from knn.core.classifier import MultiProcessedKnnClassifier as multi_processed_knn
 
 if __name__ == '__main__':
-    training_dataset = DatasetFactory.create_dataset_from_files(data_type='training')
-    test_dataset = DatasetFactory.create_dataset_from_files(data_type="test")
+    training_data_set = DatasetFactory.create_dataset_from_files(data_type='training')
+    test_data_set = DatasetFactory.create_dataset_from_files(data_type="test")
 
-    knn.classify(training_dataset=training_dataset, test_dataset=test_dataset, number_of_neighbors=10)
+    # knn.classify(training_data_set=training_data_set, test_data_set=test_data_set, number_of_neighbors=10)
+    multi_processed_knn(training_data_set=training_data_set).classify(test_data_set=test_data_set,
+                                                                      number_of_neighbors=10)
