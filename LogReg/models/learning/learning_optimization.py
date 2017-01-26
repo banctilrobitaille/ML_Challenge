e28943ec85125ae2ce3f4e3f7ffe21ea6d19572a
Antoine Dozois <dozois.a@gmail.com>
2017-01-26 20:15:18 +0100

    Adding learning algorithm

---
+++ LogReg/models/learning/learning_optimization.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class OptimizationType(object):
+    GRADIENT = "gradient"
+
+class GradientDescent(object):
+    @staticmethod
+    def compute_gradient(probability_matrix, target_matrix, feature_matrix):
+        return -(np.dot(feature_matrix.T, (target_matrix - probability_matrix))) / feature_matrix.shape[0]
+
+class UpdateWeights(object):
+    @staticmethod
+    def update_weights(weight_matrix, probability_matrix, target_matrix, feature_matrix, learning_rate):
+        weight_matrix -= learning_rate * GradientDescent.compute_gradient(probability_matrix, target_matrix, feature_matrix)
