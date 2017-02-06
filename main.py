27a35dddeb3b6d4a987ec58cee8a587255c6703c
Antoine Dozois <dozois.a@gmail.com>
2017-02-06 23:32:59 +0100

    Main for final evaluation

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ def launch_neural_network_classification():
     neural_network.classify(test_data_set)
     FileHelper.save_trained_model(neural_network, ClassificationMethod.NN)
 
+
 def launch_log_reg_classification():
     try:
         training_data_set, test_data_set = FileHelper.load_data_sets_for(ClassificationMethod.LOG_REG)
@@ -65,24 +66,13 @@ def launch_log_reg_classification():
                 classification_method=ClassificationMethod.LOG_REG)
 
     log_reg_classifier = LogRegClassifier(training_data_set, 0.1)
-    log_reg_classifier.train(number_epoch=10000, cost_threshold=0.01, debug=True)
+    log_reg_classifier.train(number_epoch=10000, cost_threshold=0.01, debug=False)
     log_reg_classifier.classify(test_data_set)
 
     FileHelper.save_trained_model(log_reg_classifier, ClassificationMethod.LOG_REG)
 
 
 if __name__ == '__main__':
-    # launch_knn_classification_with(1)
-    # launch_neural_network_classification()
-    # launch_log_reg_classification()
-    try:
-        training_data_set, test_data_set = FileHelper.load_data_sets_for(ClassificationMethod.LOG_REG)
-    except UnableToLoadDatasetException as e:
-        print(e.message)
-        training_data_set, test_data_set = DatasetFactory.create_and_save_data_set_from_files(
-            with_data_normalization=False,
-            with_threshold=True,
-            number_of_features=196,
-            classification_method=ClassificationMethod.LOG_REG)
-    gp = GaussianProcessClassifier(training_data_set)
-    gp.classify(test_data_set)
+     launch_knn_classification_with(1)
+     launch_neural_network_classification()
+     launch_log_reg_classification()
